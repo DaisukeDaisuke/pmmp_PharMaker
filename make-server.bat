da4e5609e5617a09cd018ff75a39fae5d279b058
DaisukeDaisuke <akblcmakblcm@gmail.com>
2021-03-11 09:44:58 +0900

    Update make-server.bat

--- make-server.bat
+++ make-server.bat
@@ -1,5 +1,5 @@
-git clone git@github.com:pmmp/PocketMine-MP.git -b master
+git clone git@github.com:pmmp/PocketMine-MP.git
 cd PocketMine-MP
 git submodule update --init
-..\..\..\bin\php\php.exe ..\..\bin\composer.phar make-server
+.\bin\php\php.exe .\bin\composer.phar make-server
 pause
