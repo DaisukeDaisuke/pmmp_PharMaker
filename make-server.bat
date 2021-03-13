2961c864cc2e2ceceb3a0afb918fbc59c168cd87
DaisukeDaisuke <akblcmakblcm@gmail.com>
2021-03-13 12:02:08 +0900

    Update make-server.bat

--- make-server.bat
+++ make-server.bat
@@ -1,4 +1,25 @@
-git clone --recursive git@github.com:pmmp/PocketMine-MP.git
-cd PocketMine-MP
-.\bin\php\php.exe .\bin\composer.phar make-server
-pause
+@echo off
+@rem Please download "composer.phar" from "https://getcomposer.org/composer-stable.phar".
+
+@rem git clone --recursive git@github.com:pmmp/PocketMine-MP.git
+@rem cd PocketMine-MP
+@rem .\bin\php\php.exe .\bin\composer.phar make-server
+@rem pause
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist .\bin\composer.phar (
+  git clone --recursive git@github.com:pmmp/PocketMine-MP.git
+  cd PocketMine-MP
+  ..\%PHP_BINARY% ..\bin\composer.phar make-server
+  pause
+) else (
+  echo Please download ".\bin\composer.phar" from "https://getcomposer.org/composer-stable.phar".
+  pause
+)
+
